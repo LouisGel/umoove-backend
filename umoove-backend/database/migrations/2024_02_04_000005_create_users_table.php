059ba20d2b403f5297bb02044178be7cb6d5a6d8
louis-Philippe <louis_Philippe@outlook.com>
2024-02-03 20:16:45 -0500

    Ajout des models et controllers.

--- umoove-backend/database/migrations/2024_02_04_000005_create_users_table.php
+++ umoove-backend/database/migrations/2024_02_04_000005_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->foreignId('movement_temp_id')->nullable()->constrained()->onDelete('set null');
             $table->rememberToken();
             $table->timestamps();
         });
