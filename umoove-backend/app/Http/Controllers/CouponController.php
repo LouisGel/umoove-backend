dfc51b15af35bfe88d67aa6b71b33f18c182ff61
louis-Philippe <louis_Philippe@outlook.com>
2024-02-04 01:53:05 -0500

    fix pour qte restante

--- umoove-backend/app/Http/Controllers/CouponController.php
+++ umoove-backend/app/Http/Controllers/CouponController.php
@@ -22,11 +22,18 @@ class CouponController extends Controller
 
     public function list_all(){
         $coupons = DB::query()
-            ->select('coupons.id', 'coupons.code', 'entreprises.name as entreprise_name', 'coupon_types.name', 'coupon_types.description', 'coupon_types.price', 'coupon_types.max' )
+            ->select('coupons.id',
+                'coupons.code',
+                'entreprises.name as entreprise_name',
+                'coupon_types.name',
+                'coupon_types.description',
+                'coupon_types.price',
+                DB::raw('coupon_types.max - COUNT(coupons.coupon_type_id) as remaining_coupons') )
             ->from('coupons')
             ->join('coupon_types', 'coupons.coupon_type_id', '=', 'coupon_types.id')
             ->join('entreprises', 'coupon_types.entreprise_id', '=', 'entreprises.id')
             ->whereNotNull('coupons.date_used')
+            ->groupBy('coupons.coupon_type_id', 'coupons.id', 'coupons.code', 'entreprises.name', 'coupon_types.name', 'coupon_types.description', 'coupon_types.price', 'coupon_types.max')
             ->get();
         return response()->json($coupons);
     }
